8f862125cfb3dc11655588bf9f8832019c1a2a3b
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-08-24 01:06:08 -0500

    Worked on home work

--- Conditionalstatements/Main.cpp
+++ Conditionalstatements/Main.cpp
@@ -7,7 +7,7 @@ int main()
 		char problem;
 
 		std::cout << "Class work" << std::endl;
-		std::cout << "Enter 0 to continue" << std::endl;
+		std::cout << "Enter 0 to continue to Home work" << std::endl;
 		std::cout << "Choose problem: Grade(1), Fizz Buzz(2), Calculator(3), Player Health(4)" << std::endl;
 		std::cin >> problem;
 		switch (problem)
@@ -140,45 +140,87 @@ int main()
 		{
 		case '1':
 			//Problem 1
+		{
+			int x;
+			int y = 0;
+			if (y == 0)
 			{
-				int x;
-				int y = 0;
-				if (y == 0)
-				{
-					x = 100;
-				}
-				std::cout << "The value of x is... " << x << std::endl;
-				break;
+				x = 100;
 			}
+			std::cout << "The value of x is... " << x << std::endl;
+			break;
+		}
 		case '2':
 			//Problem 2
-			{
+		{
 			int x;
+			int y;
+			
 			std::cout << "Enter x..." << std::endl;
 			std::cin >> x;
-			int y;
 			std::cout << "Enter y..." << std::endl;
 			std::cin >> y;
+			
 			if (x > y)
 			{
+				std::cout << "x is greater" << std::endl;
 				std::cout << x << std::endl;
 			}
 			else if (y > x)
 			{
+				std::cout << "y is greater" << std::endl;
 				std::cout << y << std::endl;
 			}
-				break;
+			else
+			{
+				std::cout << "x and y are equal..." << std::endl;
 			}
+			break;
+		}
 		case '3':
 			//Problem 3
-			{
-			
-			}
+		{
+
+		}
 		case '4':
 			//problem 4
+		{
+			char choice;
+			
+			std::cout << "Choose a number 1 - 4" << std::endl;
+			std::cin >> choice;
+			switch (choice)
 			{
-
+			case '1':
+				{
+					std::cout << "You choose... 1" << std::endl;
+					break;
+				}
+			case '2': case '3':
+				{
+					std::cout << "You choose... 2 or 3" << std::endl;
+					break;
+				}
+			case '4':
+				{
+					std::cout << "You choose... 4" << std::endl;
+					break;
+				}
+			default:
+				{
+				std::cout << "Invalid choice..." << std::endl;
+					break;
+				}
 			}
+			break;
+		}
+		case '5':
+			//problem 5
+		{
+
+		}
+		default:
+			break;
 		}
 	}
 	system("pause");
