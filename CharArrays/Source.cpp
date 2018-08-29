c92c9d1498377b045a6f8a44e83fd8218c4d84c5
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-08-29 12:44:36 -0500

    Worked on character arrays

---
+++ CharArrays/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int main()
+{
+	//name in intergers form
+	{
+		char name[] = "Thomas";
+		for (int i = 0; i < 6; i++)
+		{
+			std::cout << name[i] << " ";
+			std::cout << (int)name[i] << std::endl;
+		}
+	}
+	//while loop determine number of characters
+	{
+		char school[] = "AIEUS";
+		int count = 0;
+		while (school[count] != '\0')
+		{
+			count++;
+		}
+		std::cout << count << std::endl;
+	}
+	//string compare
+	{
+		char nameOne[] = "Thomas";
+		char nameTwo[] = "Thomas";
+		int lenOne = 0;
+		int lenTwo = 0;
+		bool isSame = true;
+		for (int i = 0; nameOne[i] != '\0'; i++)
+		{
+			lenOne++;
+		}
+		for (int i = 0; nameTwo[i] != '\0'; i++)
+		{
+			lenTwo++;
+		}
+		if (lenTwo != lenOne)
+			isSame = false;
+		for (int i = 0; i < lenOne; i++)
+		{
+			if (nameOne[i] == nameTwo[i])
+			{
+				continue;
+			}
+			else
+			{
+				isSame = false;
+				break;
+			}
+		}
+		if (isSame == true)
+		{
+			std::cout << "These strings are the same" << std::endl;
+		}
+		else
+		{
+			std::cout << "They are not the same" << std::endl;
+		}
+	}
+	system("pause");
+}
