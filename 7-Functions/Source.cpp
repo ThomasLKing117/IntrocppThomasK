76a62fa2a3042e575d5cc48d3a74494a5f1d551c
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-05 17:53:16 -0500

    Functions

--- 7-Functions/Source.cpp
+++ 7-Functions/Source.cpp
@@ -38,11 +38,28 @@ int arraySum(int arr[], int size)
 	return 0;
 }
 
+void revArray(int arr[], int size)
+{
+	int temp;
+	for (int i = 0; i < size / 2; i++)
+	{
+		temp = arr[i];
+		arr[i] = arr[size - 1 - i];
+		arr[size - 1 - i] = temp;
+	}
+}
+
 int main()
 {
 	print("Hello World!!!\n");
 	valuesTo10();
+	print("\n");
 	valuesBetween(10,15);
+	print("\n");
+	int myArray[4] = { 4,3,2,1 };
+	arraySum(myArray, 4);
+	print("\n");
+	revArray(myArray, 4);
 	print("\n");
 	system("pause");
 	return 0;
