07ca1f07c3e027feec75dfddb3329e287ebd2b0e
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-10 17:57:35 -0500

    Homework

--- Structures/Source.cpp
+++ Structures/Source.cpp
@@ -42,5 +42,26 @@ int main()
 
 	Stock stockSwords;
 	walGreens.swords = stockSwords;
+	walGreens.money = 1000;
+	walGreens.swords.quantity = 10;
+	walGreens.swords.item.price = 100;
+	
 	josh.Buy(walGreens.swords);
+	josh.Money = 100;
+	walGreens.money += josh.Money;
+	josh.Money -= walGreens.swords.item.price;
+	walGreens.swords.quantity -= 1;
+
+	tk.Buy(walGreens.swords);
+	tk.Money = 500;
+	walGreens.money += tk.Money;
+	tk.Money -= (walGreens.swords.item.price * 5);
+	walGreens.swords.quantity -= 5;
+
+	robRiley.Buy(walGreens.swords);
+	robRiley.Money = 400;
+	walGreens.money += robRiley.Money;
+	robRiley.Money -= (walGreens.swords.item.price * 4);
+	walGreens.swords.quantity -= 4;
+
 }
