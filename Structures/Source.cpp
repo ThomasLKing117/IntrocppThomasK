2d9fae4cd8428fd3a8d16d0338802a68ea8b005d
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-10 16:03:02 -0500

    Started structures

---
+++ Structures/Source.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <string>
+
+struct Shop
+{
+	Stock swords;
+	int money;
+};
+
+struct Buyer
+{
+	int buy(Stock, int item)
+	{
+
+	}
+	int money;
+};
+
+struct Item
+{
+	std::string name;
+	int price;
+};
+
+struct Stock
+{
+	Item item;
+	int quantity;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <iostream>
+//
+//struct Shop
+//{
+//	int sGold = 10000;
+//	int sHealthPotion = 100;
+//	int sManaPotion = 50;
+//	int sStaminaPotion = 20;
+//	int sStock[3] = { 20,20,20 };
+//	int buy(int item, int size)
+//	{
+//		for (int i = item; i < size; i++)
+//		{
+//
+//		}
+//	}
+//};
+//
+//struct Player
+//{
+//	int pGold;
+//	int pHealthPotion;
+//	int pManaPotion;
+//	int pStaminaPotion;
+//	int pStock[3];
+//};
+//
+//void money(int &amount)
+//{
+//	std::cout << "How much gold do you have?" << std::endl;
+//	std::cin >> amount;
+//	std::cout << "You have " << amount << " gold!" << std::endl;
+//	std::cout << std::endl;
+//}
+//
+//void hPotion(int &health)
+//{
+//	std::cout << "How many Health Potions do you have?" << std::endl;
+//	std::cin >> health;
+//	std::cout << "You have " << health << " Health Potions" << std::endl;	
+//	std::cout << std::endl;
+//}
+//
+//void mPotion(int &mana)
+//{
+//	std::cout << "How many Mana Potions do you have?" << std::endl;
+//	std::cin >> mana;
+//	std::cout << "You have " << mana << " Mana Potions" << std::endl;
+//	std::cout << std::endl;
+//}
+//
+//void sPotion(int &stamina)
+//{
+//	std::cout << "How many Stamina Potions do you have?" << std::endl;
+//	std::cin >> stamina;
+//	std::cout << "You have " << stamina << " Stamina Potions" << std::endl;
+//	std::cout << std::endl;
+//}
+//
+//
+//
+//int main()
+//{
+//	int startingGold = 0;
+//	int startingPotionH = 0;
+//	int startingPotionM = 0;
+//	int startingPotionS = 0;
+//
+//	money(startingGold);
+//	hPotion(startingPotionH);
+//	mPotion(startingPotionM);
+//	sPotion(startingPotionS);
+//
+//	Player Buyer;
+//	Buyer.pGold = startingGold;
+//	Buyer.pHealthPotion = startingPotionH;
+//	Buyer.pManaPotion = startingPotionM;
+//	Buyer.pStaminaPotion = startingPotionS;
+//	Buyer.pStock[3] = { startingPotionH, startingPotionM, startingPotionS };
+//	system("cls");
+//
+//
+//	
+//	return 0;
+//}
