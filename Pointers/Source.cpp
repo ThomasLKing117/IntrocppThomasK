fca851037602e205e2e6c6d95780394fd6feadba
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-12 17:25:59 -0500

    Started pointers

---
+++ Pointers/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void reverseArray(int* ptr, int size)
+{
+	int* first;
+	int* last;
+	first = ptr;
+	last = first + (size - 1);
+
+	
+	for (int i = 0; i < size; i++)
+	{
+		
+	}
+
+}
+
+int main()
+{
+	int nums[6];
+	int* ptr = &nums[0];	
+	for (int i = 0; i < 6; i++)
+	{
+		*(ptr++) = 1;
+	}
+	int myNums[3] = { 2,4,6 };
+	int* myNumsPtr = &myNums[0];
+	reverseArray(myNumsPtr, 3);
+}
