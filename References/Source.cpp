73f5e0d1a8a1c5af649afd40fc286fce9c2891f4
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-14 12:34:49 -0500

    Change

---
+++ References/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+	//problem 1
+	{
+		//b
+		int variable = 10;
+		//c
+		std::cout << "variable:: " << variable << std::endl;
+		system("pause");
+		//d
+		int& refToVariable = variable;
+		//e
+		std::cout << "refToVariable:: " << refToVariable << std::endl;
+		system("pause");
+		//f
+		variable = 117;
+		std::cout << "refToVariable:: " << refToVariable << std::endl;
+		system("pause");
+		//g
+		int& anotherRefToVariable = variable;
+		std::cout << "anotherRefToVariable:: " << anotherRefToVariable << std::endl;
+		system("pause");
+		//h
+		/*int& refToNothing;*/
+		int somethingElse = 20;
+		refToVariable = somethingElse;
+		std::cout << "somethingElse:: " << somethingElse << std::endl;
+		system("pause");
+		std::cout << "refToVariable:: " << refToVariable << std::endl;
+		std::cout << "variable:: " << variable << std::endl;
+		system("pause");
+	}
+}
