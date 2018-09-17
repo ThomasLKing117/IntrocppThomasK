4325b5b3290a13efe772329ebd5297a095fabf74
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-17 17:32:28 -0500

    !?

--- Stack/Source.cpp
+++ Stack/Source.cpp
@@ -4,14 +4,29 @@ class Stack
 {
 private: //AF
 	int values[20];
+	int count = 0;
 public:
-	bool isEmpty();
-	int top();
-	void pop();
-	void push(int pot);
+	bool isEmpty()
+	{
+
+	}
+	int top()
+	{
+
+	}
+	void pop()
+	{
+		count = count - 1;
+	}
+	void push(int value)
+	{
+		values[count] = value;
+		count = count + 1;
+	}
 };
 
 int main()
 {
-
+	Stack stack;
+	stack.push(7);
 }
