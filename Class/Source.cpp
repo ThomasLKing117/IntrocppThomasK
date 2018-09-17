21b267a24d187cd3033322d4e15c4357de92b499
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-17 15:52:32 -0500

    ???

---
+++ Class/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+//class Fighter
+//{
+//private:
+//	int m_health;
+//	int m_power;
+//public:
+//	void TakeDamage(int amount);
+//	void DoDamage(Fighter &other);
+//};
+//
+//int main()
+//{
+//	Fighter* ryu = new Fighter();
+//	ryu->DoDamage(*ryu);
+//} 
