70262b4b1b427f4f23a623ad294d155959bffd1f
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-09-26 11:48:56 -0500

    need to make teams fight

--- Lab00/Source.cpp
+++ Lab00/Source.cpp
@@ -12,16 +12,68 @@ int main()
 	
 	Hero* hero2 = new Hero;
 	hero2->GetName("Captain America");
-	
-	while (hero1->IsAlive() && hero2->IsAlive())
-		g->Battle(*hero1, *hero2);
-	
+
+	Hero* hero3 = new Hero;
+	hero3->GetName("War Machine");
+
+	Hero* hero4 = new Hero;
+	hero4->GetName("Hawkeye");
+
+	Hero* hero5 = new Hero;
+	hero5->GetName("Black Widow");
+
+	Hero* hero6 = new Hero;
+	hero6->GetName("Falcon");
+
+	Hero* hero7 = new Hero;
+	hero7->GetName("Black Panther");
+
+	Hero* hero8 = new Hero;
+	hero8->GetName("Bucky Barnes");
+
+	Hero* hero9 = new Hero;
+	hero9->GetName("The Vision");
+
+	Hero* hero10 = new Hero;
+	hero10->GetName("Ant Man");
+
+	Hero* hero11 = new Hero;
+	hero11->GetName("Spider Man");
+
+	Hero* hero12 = new Hero;
+	hero12->GetName("Scarlet Witch");
+
+	Stack team1;
+	team1.push(hero1);
+	team1.push(hero3);
+	team1.push(hero5);
+	team1.push(hero7);
+	team1.push(hero9);
+	team1.push(hero11);
+
+	Stack team2;
+	team2.push(hero2);
+	team2.push(hero4);
+	team2.push(hero6);
+	team2.push(hero8);
+	team2.push(hero10);
+	team2.push(hero12);
 	
 	system("pause");
 	
 	return 0;
 }
-/*Hero* hero3 = new Hero;
+/*
+	Hero* hero1 = new Hero;
+	hero1->GetName("Iron Man");
+	
+	Hero* hero2 = new Hero;
+	hero2->GetName("Captain America");
+
+	while (hero1->IsAlive() && hero2->IsAlive())
+		g->Battle(*hero1, *hero2);
+
+	Hero* hero3 = new Hero;
 	hero3->GetName("War Machine");
 
 	Hero* hero4 = new Hero;
